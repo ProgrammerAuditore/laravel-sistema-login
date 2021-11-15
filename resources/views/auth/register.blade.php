dbd93c867798d87a30130226e2876433372026bf
ProgrammerAuditore <victorvj098@gmail.com>
2021-11-15 16:31:44 -0600

    ***** se agrego formularios en la vista register y login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,5 +3,29 @@
 @section('title', 'Register')
 
 @section('content')
-    <h1 class="text-5xl text-center pt-24">Register</h1>
+    <div class="block mx-auto my-12 p-8 bg-white w-1/3 border border-gray-200 rounded-lg shadow-lg">
+    <h1 class="text-3xl text-center font-bold">Register</h1>
+    <form action="" class="mt-4" method="POST">
+        
+        <p class="border border-red-500 rounded-md bg-red-100 w-full text-red-600 p-2 my-2">
+            * Errores 
+        </p>
+
+        <input class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg placeholder-gray-900 p-2 my-2 focus:bg-white"
+        type="text" id="name" name="name" placeholder="Name">
+        
+        <input class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg placeholder-gray-900 p-2 my-2 focus:bg-white"
+        type="email" id="email" name="email" placeholder="Email">
+
+        <input class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg placeholder-gray-900 p-2 my-2 focus:bg-white"
+        type="password" id="password" name="password" placeholder="Password">
+
+        <input class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg placeholder-gray-900 p-2 my-2 focus:bg-white"
+        type="password" id="password_confirmation" name="password_confirmation" placeholder="Password confirmation">
+
+        <input class="rounded-md bg-indigo-500 w-full text-lg text-white font-semibold p-2 my-3 hover:bg-indigo-600"
+        type="submit" value="Send">
+
+    </form>
+</div>
 @endsection
